7fee4003cf600792da3fa681a2efa7c19e8e6829
jontysh4w <jontyshaw@btinternet.com>
2024-07-27 19:13:35 +0100

    adding images and links for other projects

--- js/script.js
+++ js/script.js
@@ -15,10 +15,11 @@ const portfolioData = {
             image: "assets/float_point_arithmetic_tool.png"
         },
         {
-            name: "Python tool for hunting rental properties in London",
+            name: "Python tool combining Google Sheets, NLP and Web Scraping",
             description: `Leveraging goole API's and webscraping to extract useful information and process 
             tenant relevant data such as time to work in the morning. Data is then pushed to a google sheet.`,
-            github: "https://github.com/JSh4w/House_hunter"
+            github: "https://github.com/JSh4w/House_hunter",
+            image: "assets/SiteToSheet.png"
         },
         {
             name: "Stock analysis MERN application",
@@ -29,7 +30,8 @@ const portfolioData = {
         {
             name: "System Identification and Model-Based Reinforcement Learning for Control of a Soft Robotic Manipulator",
             description: "4th Year project developing machine learning and neural networks to control a soft pendulum",
-            detailPage: "project_pages/4m25.html"
+            link: "project_pages/4M25_Robotics.pdf",
+            image: "assets/4M25_robotics.png"
         }        
     ]
 };
