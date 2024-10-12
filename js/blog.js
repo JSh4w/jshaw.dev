01a12cd0143562188596f16004a194f445e9b644
jontysh4w <jontyshaw@btinternet.com>
2024-10-12 16:17:46 +0100

    creating blog page and first post

--- js/blog.js
+++ js/blog.js
@@ -1,23 +1,11 @@
 // Links data structure
 const linksData = [
     {
-        name: "Roadmaps",
-        url: "https://roadmap.sh/",
+        name: "Interesting Links and Resources",
+        url: "interesting_links.html",
         description: `A powerful website that provides consice information about different careers in tech.
         While the information is not in full it provides a fun "guide" style approach that users can check off.`
-    }, 
-    {
-        name: "First Timers Only Blog",
-        url: "https://kentcdodds.com/blog/first-timers-only",
-        description: `A blog by Kent C. Dodds that aims to help first time programmers get onto open source projects.
-        Namely, I believe this helped start the "first-timers-only" tags on Github along with subesquent variations of the tag.`
-    },
-    {
-        name: "Stack Overflow Developer Survey",
-        url: "https://survey.stackoverflow.co/2023/#productivity-impacts-industry-ic",
-        description: `In May 2023 over 90,000 developers responded to our annual survey about how they learn and level up,
-        which tools they're using, and which ones they want.`
-    },
+    }
 ];
 /**
  * Still to add : https://refine.dev/
