a76b45a2e1b4a5a7aeea4b32117b4db5509ce38f
JSh4w <jontyshaw@btinternet.com>
2025-04-21 10:05:19 +0100

    fix: added constant to remove repeated paths feat: added Animejs to interesting links page

--- js/blog_posts/interesting_links.js
+++ js/blog_posts/interesting_links.js
@@ -1,25 +1,32 @@
 // Links data structure
+const IMAGE_BASE_PATH = "../assets/interesting_links" ;
 const linksData = [
+    { 
+        name: "Animejs",
+        url: "https://animejs.com/",
+        description: "A animation library for use with React, one of the most impressive demonstrations of dynamic animations on a static web page",
+        image: `${IMAGE_BASE_PATH}/animejs.png`
+    },
     {
         name: "Roadmaps",
         url: "https://roadmap.sh/",
         description: `A powerful website that provides consice information about different careers in tech.
         While the information is not in full it provides a fun "guide" style approach that users can check off.`,
-        image: "../assets/interesting_links/roadmap.png"
+        image: `${IMAGE_BASE_PATH}/roadmap.png`
     }, 
     {
         name: "First Timers Only Blog",
         url: "https://kentcdodds.com/blog/first-timers-only",
         description: `A blog by Kent C. Dodds that aims to help first time programmers get onto open source projects.
         Namely, I believe this helped start the "first-timers-only" tags on Github along with subesquent variations of the tag.`,
-        image: "../assets/interesting_links/first_timers_only.png"
+        image: `${IMAGE_BASE_PATH}/first_timers_only.png`
     },
     {
         name: "Stack Overflow Developer Survey",
         url: "https://survey.stackoverflow.co/2023/#productivity-impacts-industry-ic",
         description: `In May 2023 over 90,000 developers responded to our annual survey about how they learn and level up,
         which tools they're using, and which ones they want.`,
-        image: "../assets/interesting_links/stack_overflow_developer_survey.png"
+        image: `${IMAGE_BASE_PATH}/stack_overflow_developer_survey.png`
     },
 ];
 /**
